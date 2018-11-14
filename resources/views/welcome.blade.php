8808e758aa536118cb5eb4be2f3cd03ba33cb0ce
hasetennisth <hasetennisth@gmail.com>
2018-11-14 18:21:50 +0900

    Update resources/views/welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
                 </div>
             @endif
 
-            <div class="content">
+            <div class="content">b
                 <div class="title m-b-md">
                     Laravelbbbbb
                 </div>
