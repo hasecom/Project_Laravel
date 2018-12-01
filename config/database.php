3ee678cfae04bf063b24432af697e35f7fdda752
YutaIto <amakano1587@gmail.com>
2018-12-01 11:15:22 +0900

    Update database.php

--- config/database.php
+++ config/database.php
@@ -42,11 +42,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', '172.19.47.241'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'LAA0934711-project'),
+            'username' => env('DB_USERNAME', 'LAA0934711'),
+            'password' => env('DB_PASSWORD', 'tridentwifi'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
