a19ae35d1e53215d926b0445e9eb2a1517a5f215
hasetennisth-sub <hasetennis52@gmail.com>
2018-12-05 09:37:10 +0900

    コメントアウト多く

--- app/Admin.php
+++ app/Admin.php
@@ -25,11 +25,18 @@ class Admin extends Model
     return $cold;
     }
 
-  
+  /*
+(array)XXX or エンコード後にデコード
+  (array)XXXの方がスピードは速いが
+  ネスト構造になっているものは結果が変わるかも。
+  */
+
+    //エンコード
     protected function en($array){
         $json = json_encode($array,JSON_PRETTY_PRINT);
         return $json;
     }
+    //デコード
     private function de($json){
         $array = json_decode($json,true);
         return $array;
