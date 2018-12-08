03aa7a010795ca99f3299b4bf3277da48a69d62b
hasetennisth-sub <hasetennis52@gmail.com>
2018-12-09 01:23:57 +0900

    詳細modal追加、どうでも良いUI

--- resources/js/components/admin_folder/admin_manager.vue
+++ resources/js/components/admin_folder/admin_manager.vue
@@ -12,22 +12,54 @@
                 <th>管理者レベル</th>
             </tr>
             <tbody>
-            <tr v-for="item in admins" :key="item.id">
+            <tr v-for="item in admins" :key="item.id" v-on:click="select(item)">
                 <td v-text="item.id"></td>
                 <td v-text="item.admin_id"></td>
                   <td v-text="item.admin_pw"></td>
                 <td v-text="item.admin_authority"></td>
-               
             </tr>
             </tbody>
         </table>
     </div>
+
+ <!-- コンポーネント MyModal -->
+    <MyModal @close="closeModal" v-if="admin_modal" class="border-0">
+     <div class="modal_display">
+      
+         <table class="table table-hover">
+           
+           <caption>{{admin_fnc().admin_id}}</caption>
+    
+           <tbody>
+            <tr>
+                <th>管理者内部ID</th>
+                <td>{{admin_fnc().id}}</td>
+            </tr>
+            <tr>
+              <th>管理者ID</th>
+              <td>{{admin_fnc().admin_id}}</td>
+            </tr>
+            <tr>
+              <th>管理者パスワード</th>
+              <td>{{admin_fnc().admin_pw}}</td>
+            </tr>
+            <tr> 
+              <th>管理者レベル</th>
+              <td>{{admin_fnc().admin_authority}}</td>
+            </tr>
+            </tbody>
+        </table>
+       
+      </div>
+    </MyModal>
+
     </div>
   </div>
   
 </template>
 
 <script>
+import MyModal from './admin_modal/admin_manager_modal.vue'
 window.Vue = require('vue');
 
 //親要素(main)からデータをpropsで取得
@@ -37,18 +69,44 @@ export default {
       admin_info_str: {
         type:String,
       }
-      },
+      },components: { MyModal },
       data(){
     return{
      admin_info:this.admin_info_str,
-     admins:[]
+     admins:[],
+      admin_modal: false,
+      admin_data:""
     }
   },   
   methods: {
-         get_admins_info() {
+      get_admins_info() {
            this.admins = JSON.parse(this.admin_info);
-        }
+        },
+
+       openModal() {
+      this.admin_modal = true
     },
+      closeModal() {
+      this.admin_modal = false
+    },
+      admin_fnc(){
+       return this.admin_data;
+      }
+    
+    },
+    computed: {
+      select: function () {
+            self = this;
+        return function (details_user) {
+          this.openModal();
+           this.admin_data = details_user;
+          
+        };
+            
+         
+    }
+    },
+
     mounted() {
         this.get_admins_info();
     },
@@ -62,6 +120,16 @@ export default {
 
 
 </script>
-
 <style>
+.modal_display{
+  background:white;
+  padding:50px;
+}
+
+caption {
+caption-side:top;
+text-align: center;
+font-weight: bold;
+font-size:x-large;
+}
 </style>
---
+++ resources/js/components/admin_folder/admin_modal/admin_manager_modal.vue
@@ -0,0 +1,52 @@
+<template>
+  <transition name="modal" appear>
+    <div class="modal modal-overlay" @click.self="$emit('close')">
+      <div class="modal-window ">
+             <slot name="header">
+            <button @click="$emit('close')" class="float-right btn-lg btn-light"><i class="fas fa-times"></i></button>
+          </slot>
+        <div class="rounded shadow border">
+          <slot/>
+        </div>
+      
+       
+    
+      </div>
+    </div>
+  </transition>
+</template>
+
+<style>
+.modal {
+    
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: fixed;
+    z-index: 30;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.2);
+  
+
+}
+
+
+
+.modal-enter-active, .modal-leave-active {
+  transition: opacity 0.2s;
+
+}
+
+
+.modal-leave-active {
+  transition: opacity 0.2s ease 0.1s;
+}
+
+.modal-enter, .modal-leave-to {
+  opacity: 0;
+
+}
+</style>
