6adaf3ad2ad0307630e240b1f4247c1bc42d8789
YutaIto <amakano1587@gmail.com>
2019-01-25 09:27:21 +0900

    タイムライン詳細画像表示

--- resources/js/components/user_folder/individual/component/user_post_details.vue
+++ resources/js/components/user_folder/individual/component/user_post_details.vue
@@ -6,7 +6,7 @@
                             <div class="col-md-5">
                               <div>{{photo_data.user_name}}&emsp;&emsp;<span class="text-muted">@{{photo_data.user_id}}</span></div>
                               <!-- <span class="bd-placeholder-img card-img-top" width="100%" height="300" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: カードの画像"><title>プレースホルダ</title> -->
-			                    <amplify-s3-image style="pointer-events: none;" :imagePath= "photo_data.photo_path+'/'+photo_data.file_name+'.png'" ></amplify-s3-image>
+			                    <amplify-s3-image style="pointer-events: none;" :imagePath= "'Photos/'+photo_data.photo_path+'/'+photo_data.file_name+'.png'" ></amplify-s3-image>
                               <!-- </span> -->
                                     <!--<svg class="bd-placeholder-img card-img-top" width="100%" height="300" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: カードの画像"><title>プレースホルダ</title><rect fill="#868e96" width="100%" height="100%"></rect><text fill="#dee2e6" dy=".3em" x="30%" y="50%">no image</text></svg>-->
 			                        <div class="card-body px-2 py-3">
